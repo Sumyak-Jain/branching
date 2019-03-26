88cb560efac3b6a023fa33b829e13bde1c7cd921
sumyak jain <3456gaurav@gmail.com>
2019-03-26 10:07:49 +0530

    commit1

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+// C++ program to create calculator using 
+// switch statement 
+#include <iostream> 
+using namespace std; 
+  
+// Main program 
+main() 
+{ 
+    char op; 
+    float num1, num2; 
+  
+    // It allows user to enter operator i.e. +, -, *, / 
+    cin >> op; 
+  
