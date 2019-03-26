36881db53dae6ad86175f7c9094b6a7e786a3a3b
sumyak jain <3456gaurav@gmail.com>
2019-03-26 10:21:40 +0530

    commit4

--- calculator.cpp
+++ calculator.cpp
@@ -25,4 +25,22 @@ main()
         case '-': 
             cout << num1 - num2; 
             break; 
-           
+         // If user enter * 
+        case '*': 
+            cout << num1 * num2; 
+            break; 
+          
+        // If user enter / 
+        case '/': 
+            cout << num1 / num2; 
+            break; 
+          
+        // If the operator is other than +, -, * or /,  
+        // error message will display 
+        default: 
+            cout << "Error! operator is not correct"; 
+            break; 
+    } // switch statement ends 
+  
+    return 0;  
+}   
